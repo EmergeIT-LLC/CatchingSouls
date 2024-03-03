0cadc1452227effe1368b8e0a4e9e0ba2428a787
Jonathan R Dameus <126197399+Jon204D@users.noreply.github.com>
2024-03-03 06:33:24 -0500

    Minor Changes

--- server/index.js
+++ server/index.js
@@ -19,19 +19,6 @@ const sessionStore = new SqlDbStore({
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  session({
-    key: 'BibleTriviaSessionCookies',
-    secret: '3B4F7C4E6DA85A5176758B437A22A',
-    store: sessionStore,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24,
-      secure: true,
-    },
-  })
-);
 
 app.get('/', (req, res) => {
   res.send("The server is running successfully. <br/>The server is running on port " + port + "... <br/>The server url is " + host + "...")
