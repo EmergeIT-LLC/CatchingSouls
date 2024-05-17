7ab32bcde7e5cf588dcbc758215b963fdb2b9853
Jonathan R Dameus <126197399+Jon204D@users.noreply.github.com>
2024-05-17 01:24:24 -0400

    Update CookieCheck.js

--- client/src/Functions/CookieCheck.js
+++ client/src/Functions/CookieCheck.js
@@ -1,4 +1,5 @@
 import Cookies from 'js-cookie';
+import {CheckUser} from '../Functions/VerificationCheck';
 
 export const CookieCheck = (name, value, options) => {
     const cookieOptions = {
@@ -9,4 +10,28 @@ export const CookieCheck = (name, value, options) => {
     };
 
     Cookies.set(name, value, cookieOptions);
+};
+
+// Function to check if a cookie exists and is not expired
+export const isCookieValid = () => {
+    let cookieName = 'csAuthServices-' + CheckUser();
+    const cookieValue = Cookies.get(cookieName);
+    if (cookieValue) {
+        // Cookie exists, now check if it's expired
+        const expirationDate = getCookieExpiration(cookieName);
+        if (expirationDate) {
+            const currentTime = new Date();
+            return currentTime < expirationDate; // True if current time is before expiration
+        }
+    }
+    return false; // Cookie doesn't exist or is expired
+};
+
+// Function to get the expiration date of a cookie
+const getCookieExpiration = (cookieName) => {
+    const expirationDateString = Cookies.get(cookieName);
+    if (expirationDateString) {
+        return new Date(expirationDateString);
+    }
+    return null; // Expiration date not found
 };
